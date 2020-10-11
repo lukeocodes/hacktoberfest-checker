b4d125a2b855ac8da1021a0f850d2c06866ab189
Luke Oliff <luke@lukeoliff.com>
2020-10-11 18:24:42 +0100

    realising css variables persist into components IS EVERYTHING

--- components/Announcement.vue
+++ components/Announcement.vue
@@ -14,29 +14,16 @@
 <style scoped>
 .Announcement__Link {
   @apply block w-full py-4 pl-16 text-sm font-bold;
-  background-color: #ff8ae2;
-  color: #1d2c4e;
+  background-color: var(--bg-contrast);
+  color: var(--color-contrast);
 }
 
 .Link__Button {
   @apply bg-white py-2 px-4 mr-4 rounded uppercase;
+  color: var(--color-contrast);
 }
 
 .Announcement__Link:hover .Link__Text {
-  @apply underline;
-}
-
-.dark-mode .Announcement__Link {
-  @apply block w-full py-4 pl-16 text-sm font-bold;
-  background-color: #93c2db;
-  color: #072540;
-}
-
-.dark-mode .Link__Button {
-  @apply bg-white py-2 px-4 mr-4 rounded uppercase;
-}
-
-.dark-mode .Announcement__Link:hover .Link__Text {
   @apply underline;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,9 @@ export default {
   },
 
   // Content module configuration (https://go.nuxtjs.dev/config-content)
-  content: {},
+  content: {
+    liveEdit: false,
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
