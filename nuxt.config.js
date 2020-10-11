e157f9ea38eec4128a8f6cbfcd0dd0d3ea44ecf5
Luke Oliff <luke@lukeoliff.com>
2020-10-11 22:04:53 +0100

    update the title description and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,13 +10,18 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hacktoberfest-checker',
+    title: '[*] Hacktoberfest Repository Checker',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Hacktoberfest Repository Checker - Unofficial Site. Hacktoberfest presented by DigitalOcean',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
