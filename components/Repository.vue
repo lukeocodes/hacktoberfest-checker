2953c3cbc197e9fe342db8e1e96b01d76edff95c
Luke Oliff <luke@lukeoliff.com>
2020-10-12 00:06:59 +0100

    bigger status icons

--- components/Repository.vue
+++ components/Repository.vue
@@ -3,10 +3,10 @@
     <div>
       <div class="Repository__Row">
         <h3 class="flex Repository__Name">
-          <svg v-if="theyHacktoberfest" class="w-4 h-4 mr-2 text-teal-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <svg v-if="theyHacktoberfest" class="w-6 mr-2 text-teal-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
           </svg>
-          <svg v-else class="w-4 h-4 mr-2 text-red-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <svg v-else class="w-6 mr-2 text-red-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
           </svg>
           <a :href="repo.url">{{ repo.name }}</a>
