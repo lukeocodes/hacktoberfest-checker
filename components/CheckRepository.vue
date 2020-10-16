2eba8e618fdf6b1bc2cdab708bef3ed0681d3480
Alhassan <23234466+alhassanv@users.noreply.github.com>
2020-10-16 11:01:21 +0300

    Allow user/repo format (#50)
    * feature: support owner/repository shaped requests
    
    * style: update from single to double quotes
    
    * refactor: rename variables and improve logic

--- components/CheckRepository.vue
+++ components/CheckRepository.vue
@@ -203,10 +203,15 @@ export default {
         return this.errors.push({message: 'Please enter a url to check.'});
       }
 
-      var isGitHub = new RegExp("https?:\\/\\/(.+?\\.)?github\\.com(\\/[A-Za-z0-9\\-\\._~:\\/\\?#\\[\\]@!$&'\\(\\)\\*\\+,;\\=]*)?");
+      var isGithub = new RegExp("https?:\\/\\/(.+?\\.)?github\\.com(\\/[A-Za-z0-9\\-\\._~:\\/\\?#\\[\\]@!$&'\\(\\)\\*\\+,;\\=]*)?");
+      var isGithubRepo = new RegExp("[a-zA-Z]+\/[a-zA-Z]+");
 
-      if (false === isGitHub.test(this.url)) {
-        return this.errors.push({message: 'Please use a GitHub url to check.'});
+      if(!isGithub.test(this.url)) {
+        this.url = `https://github.com/${this.url}`;
+      }
+
+      if (!isGithub.test(this.url) && !isGithubRepo.test(this.url)) {
+        return this.errors.push({ message: 'Please use a GitHub url to check.' });
       }
 
       this.processing = true
